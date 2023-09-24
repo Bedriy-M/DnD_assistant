8515415e5d1484cc44af7efe6cc359563f8be970
BedriyM <bedriy.m@gmail.com>
2023-09-24 23:54:21 +0300

    part2

--- test1/lib/main.dart
+++ test1/lib/main.dart
@@ -1,20 +1,35 @@
-import 'package:flutter/material.dart';
+
+/*class Song{
+  String title = "";
+  String artist = "";
+  Duration duration = Duration(seconds: 0);
+  String year = "";
+  }*/
+
+class Song with SearchMixin {
+}
+mixin SearchMixin
+  String title = "";
+  String artist = "";
+  Duration duration = Duration(seconds: 0);
+  int year = 0;
+
+  Song (this.title, this.artist, this.duration, this.year);
+
+  @override
+  bool matches(String query) {
+    return title.contains(query) || age == int.parse(query);
+  }
+}
 
 void main() {
+  Song song1 = Song(
+    title: "Song1",
+    artist: "Artist1"
+    duration: Duration(minutes: 1, seconds: 15)
+    year: 2023
+  )
 }
 
-class Song{
-  final String title = "";
-  final String artist = "";
-  final Duration duration = Duration(seconds: 0);
-  final String year = "";
-  }
 
-class Playlist{
-  final String name = "Playlist";
-
-  void addSong(Song song) => songs.add(song);
-
-  void removeSong(Song song) => songs.remove(song);
-}
 
