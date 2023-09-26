a5edd5f4ab913e126a163a3467bc4ccc1a89dd60
BedriyM <bedriy.m@gmail.com>
2023-09-26 23:32:47 +0300

    part3

--- test1/lib/main.dart
+++ test1/lib/main.dart
@@ -1,26 +1,36 @@
 
-/*class Song{
-  String title = "";
-  String artist = "";
-  Duration duration = Duration(seconds: 0);
-  String year = "";
-  }*/
-
-class Song with SearchMixin {
-}
-mixin SearchMixin
+class Song{
   String title = "";
   String artist = "";
   Duration duration = Duration(seconds: 0);
   int year = 0;
+  }
 
-  Song (this.title, this.artist, this.duration, this.year);
+
+class Playlist with SearchMixin {
+  final List<Song> songs = [];
+
+  Playlist(List<Song> songs) {
+    this.songs.addAll(songs);
+  }
+
+  // Пошук пісень у плейлисті
+  List<Song> search(String query) {
+    return songs.where((song) => song.title.contains(query)).toList();
+    songs.where((song) => song.artist.contains(query)).toList();
+  }
+}
+
+mixin SearchMixin
+  /*String title = "";
+  String artist = "";
+  Duration duration = Duration(seconds: 0);
+  int year = 0;
 
   @override
   bool matches(String query) {
     return title.contains(query) || age == int.parse(query);
-  }
-}
+  }*/
 
 void main() {
   Song song1 = Song(
@@ -33,3 +43,85 @@ void main() {
 
 
 
+Playlist playlist = Playlist([
+  Song(title: "Song1", artist: "Artist1"),
+  Song(title: "Song2", artist: "Artist2"),
+
+]);
+
+List<Song> results = playlist.search("Song1");
+
+for (Song result in results) {
+  print(result.title);
+}
+
+
+/*
+mixin SearchMixin {
+  final List<Song> songs = [];
+
+  List<Song> search(String query) {
+    return songs.where((song) {
+      return song.title.toLowerCase().contains(query.toLowerCase()) ||
+          song.artist.toLowerCase().contains(query.toLowerCase());
+    }).toList();
+  }
+}
+
+class Song {
+  final String title;
+  final String artist;
+  final Duration duration;
+  final int year;
+
+  Song({
+    required this.title,
+    required this.artist,
+    required this.duration,
+    required this.year,
+  });
+}
+
+class Playlist with SearchMixin {
+  final List<Song> songs = [];
+
+  Playlist(List<Song> songs) {
+    this.songs.addAll(songs);
+  }
+
+  // Пошук пісень у плейлисті
+  List<Song> search(String query) {
+    return songs.where((song) {
+      return song.title.toLowerCase().contains(query.toLowerCase()) ||
+          song.artist.toLowerCase().contains(query.toLowerCase());
+    }).toList();
+  }
+}
+
+void main() {
+  // Створити список пісень
+  List<Song> songs = [
+    Song(title: "Song1", artist: "Artist1"),
+    Song(title: "Song2", artist: "Artist2"),
+    Song(title: "Song3", artist: "Artist3"),
+  ];
+
+  // Створити плейлист
+  Playlist playlist = Playlist(songs);
+
+  // Знайти пісні по назві
+  List<Song> results = playlist.search("Song1");
+
+  // Вивести результати пошуку
+  for (Song result in results) {
+    print(result.title);
+  }
+
+  // Знайти пісні по виконавцю
+  results = playlist.search("Artist2");
+
+  // Вивести результати пошуку
+  for (Song result in results) {
+    print(result.title);
+  }
+}
